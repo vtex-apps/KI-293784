7221b55ba0aeac07bda9ec7c2e83f401f6c2a119
Your Name <bonacchigerman@gmail.com>
2022-04-20 16:59:16 -0300

    add and remove Loading

--- KI-293784.js
+++ KI-293784.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   const style = document.createElement("style");
   style.innerHTML = `
-  .spinner {
+  #spinner {
     border: 4px solid rgba(0, 0, 0, 0.1);
     width: 36px;
     height: 36px;
@@ -23,41 +23,53 @@ $(document).ready(function () {
   `;
   document.head.appendChild(style);
 
+  function addLoading() {
+    const scheduledDeliveryList = document.querySelector(
+      ".vtex-omnishipping-1-x-scheduledDeliveryList"
+    );
+    scheduledDeliveryList.classList.add("vtex-omnishipping-1-x-hide");
+    const spinner = document.createElement("div");
+    spinner.setAttribute("id", "spinner");
+    scheduledDeliveryList.parentElement.append(spinner);
+  }
+  
+  function removeLoading() {
+    const scheduledDeliveryList = document.querySelector(
+      ".vtex-omnishipping-1-x-scheduledDeliveryList"
+    );
+    scheduledDeliveryList.classList.remove("vtex-omnishipping-1-x-hide");
+    const spinnerElement = document.querySelectorAll("#spinner");
+    spinnerElement.forEach((e) => e.remove());
+  }
+  
   function changeDeliveryWindow() {
     vtexjs.checkout
       .getOrderForm()
       .then((orderForm) => {
-        const scheduledDeliveryList = document.querySelector(
-          ".vtex-omnishipping-1-x-scheduledDeliveryList"
-        );
-        const spinner = document.createElement("div");
-        spinner.className = "spinner";
-        scheduledDeliveryList.parentElement.append(spinner);
-        
-        scheduledDeliveryList.classList.add("vtex-omnishipping-1-x-hide");
         var shippingData = orderForm.shippingData;
         const selectedSla = shippingData.logisticsInfo[0].selectedSla;
-
-        var checkDeliveryWindow = false;
+        var hasDeliveryWindow = false;
         shippingData.logisticsInfo[0].slas.forEach((sla) => {
           if (sla.id === selectedSla) {
             if (sla.deliveryWindow) {
               sla.deliveryWindow = null;
-              checkDeliveryWindow = true;
+              hasDeliveryWindow = true;
             }
           }
         });
-        if (checkDeliveryWindow) {
+
+        if (hasDeliveryWindow) {
+          addLoading();
           shippingData.logisticsInfo[0] = {
             ...shippingData.logisticsInfo[0],
             selectedSla: null,
           };
           return vtexjs.checkout.sendAttachment("shippingData", shippingData);
-        } else {
-          scheduledDeliveryList.classList.remove("vtex-omnishipping-1-x-hide");
         }
+        return null;
       })
       .done(function () {
+        console.log("Done");
         const schedulerTongle = document.querySelector(
           "#scheduled-delivery-delivery"
         );
@@ -68,16 +80,13 @@ $(document).ready(function () {
         ) {
           setTimeout(() => {
             schedulerTongle.click();
-            const scheduledDeliveryList = document.querySelector(
-              ".vtex-omnishipping-1-x-scheduledDeliveryList"
-            );
-            scheduledDeliveryList.classList.remove(
-              "vtex-omnishipping-1-x-hide"
-            );
+            removeLoading();
           }, 1000);
         }
       });
   }
+
+  
 
   const observerScheduler = new MutationObserver((mutations, obsS) => {
     const schedulerTongle = document.querySelector(
@@ -94,6 +103,7 @@ $(document).ready(function () {
           )
         ) {
           console.log("Apagar");
+
           changeDeliveryWindow();
         } else {
           console.log("Prender");
